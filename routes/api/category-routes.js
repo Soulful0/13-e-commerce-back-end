2c1d8e4bab1ffbf608abb77943cf0df17246a76b
Soul <joshgingold336@gmail.com>
2024-05-20 12:21:17 -0500

    finished tag route javascript, bug fixes, testing phase

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -57,6 +57,7 @@ router.put("/:id", async (req, res) => {
       res.status(200).json(categoryData);
     } else {
       res.status(404).json({ message: "No category found with that id" });
+      return;
     }
   } catch (err) {
     res.status(500).json(err);
